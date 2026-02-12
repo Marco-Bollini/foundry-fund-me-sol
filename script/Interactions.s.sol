5fcb32c7668160ae82d729036f099649bdfb7c42
Marco Bollini <mabollini@fi.uba.ar>
2026-02-12 20:48:47 -0300

    .

---
+++ script/Interactions.s.sol
@@ -0,0 +1,43 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.18;
+
+import {Script, console} from "forge-std/Script.sol";
+import {DevOpsTools} from "foundry-devops/src/DevOpsTools.sol";
+import {FundMe} from "../../src/FundMe.sol";
+
+contract FundFundMe is Script {
+    uint256 constant SEND_VALUE = 0.01 ether;
+
+    function fundFundMe(address mostRecentlyDeploy) public {
+        FundMe(payable(mostRecentlyDeploy)).fund{value: SEND_VALUE}();
+        console.log("Funded FundMe with %s", SEND_VALUE);
+    }
+
+    function run() external {
+        address mostRecetlyDeploy = DevOpsTools.get_most_recent_deployment(
+            "FundMe",
+            block.chainid
+        );
+        vm.startBroadcast();
+        fundFundMe(mostRecetlyDeploy);
+        vm.stopBroadcast();
+    }
+}
+
+contract WithdrawFundMe is Script {
+    function withdrawFundMe(address mostRecentlyDeploy) public {
+        vm.startBroadcast();
+        FundMe(payable(mostRecentlyDeploy)).withdraw();
+        vm.stopBroadcast();
+    }
+
+    function run() external {
+        address mostRecetlyDeploy = DevOpsTools.get_most_recent_deployment(
+            "FundMe",
+            block.chainid
+        );
+        vm.startBroadcast();
+        withdrawFundMe(mostRecetlyDeploy);
+        vm.stopBroadcast();
+    }
+}
